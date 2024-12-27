c870f44b470c54a5500a5a4a3e308e50c865b997
qweArteom <kurylenko2artem@gmail.com>
2024-12-27 12:21:45 +0200

    first commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, redirect, url_for, flash
+from app.forms import RegistrationForm, LoginForm
+from app import app
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Для тесту додамо базову логіку
+        if form.email.data == "test@test.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
